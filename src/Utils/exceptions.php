f8139bdd70f107f7984451d20ad4b326ce8044f6
chemiX <iam@chemix.cz>
2015-10-02 00:35:14 +0200

    Shortcuts & Exceptions: init

---
+++ src/Utils/exceptions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Honza Cerny (http://honzacerny.com)
+ */
+
+namespace Aprila;
+
+/**
+ * The exception that is thrown when is no writed to db because unique key
+ */
+class DuplicateEntryException extends \RuntimeException
+{
+
+}
